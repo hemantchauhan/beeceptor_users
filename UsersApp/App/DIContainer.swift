1d96776681806691d76daa4b0f3e5a4c5cb73fdc
hemant chauhan <hchauhan@medocity.com>
2025-11-05 07:43:13 +0530

    Initial project setup: basic folder structure with minimal code.

---
+++ UsersApp/App/DIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  DIContainer.swift
+//  UsersApp
+//
+//  Created by ICHI14 on 05/11/25.
+//
+
+import Foundation
---
+++ UsersApp/App/UsersAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  UsersAppApp.swift
+//  UsersApp
+//
+//  Created by ICHI14 on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Users List View here")
+        }
+    }
+
+}
