63932e24148369b54633b4010f9c0435755b9f93
hemant chauhan <hchauhan@medocity.com>
2025-11-06 09:26:51 +0530

    app - updated UserMapper

--- UsersApp/Modules/Data/Mappers/UserMapper.swift
+++ UsersApp/Modules/Data/Mappers/UserMapper.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct UserDataMapper {
+    static func map(_ data: UserData) -> User {
+        User(id: data.id, name: data.name, username: data.username, email: data.email, photo: data.phone)
+    }
+    
+    static func mapToDetail(_ data: UserData) -> UserDetail {
+        UserDetail(id: data.id, company: data.company, address: data.address, zip: data.zip, state: data.state, country: data.country, phone: data.phone)
+    }
+}
